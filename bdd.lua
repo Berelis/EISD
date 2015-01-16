10fc2ab36f45b0c63bea16c08ebec9e19b2ec365
rjayez <romain.jayez@gmail.com>
2015-01-16 11:49:46 +0100

    Structure bdd

---
+++ bdd.lua
@@ -0,0 +1,56 @@
+pays={
+	france  = {
+		langue = {"français"},
+		monnaie = "euro",
+		religion ={""},
+		position = {"Europe de l'Ouest"},
+		pays_frontalier = {"belgique", "espagne", "allemagne", "suisse", "italie", "luxembourg", "andorre", "monaco", "liechtenstein"},
+		population = "66,6 millions",
+		regime = "république constitutionnelle",
+		continent = "Europe",
+		superficie = "Over 9000",
+		
+
+	}
+	hyrule  = {
+		langue = {"hylien"},
+		monnaie = "rubis",
+		religion ={"polythéiste"},
+		position = {"DTC"},
+		pays_frontalier = {"royaume champignon", "zebes", "kanto"},
+		population = "69 420",
+		regime = "monarchie",
+		continent = "nintendo land",
+		superficie = "987654 km2",
+		
+
+	}
+
+	twitch  = {
+		langue = {"Kappa"},
+		monnaie = "FrankerZ",
+		religion ={"Helix Fossil"},
+		position = {"4chan"},
+		pays_frontalier = {"reddit", "facebook", "twitter", "youporn", "redtube"},
+		population = "Over 9000",
+		regime = "Anarchy",
+		continent = "Cosmo",
+		superficie = "1 8 1 0 ",
+		
+
+	}
+
+	modele  = {
+		langue = {},
+		monnaie = "",
+		religion ={},
+		position = {},
+		pays_frontalier = {},
+		population = "",
+		regime = "",
+		continent = "",
+		superficie = "",
+		
+
+	}
+}
