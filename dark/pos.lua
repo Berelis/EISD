d684e65bc766c03d787461f5f58d9852a50b458b
Berelis <mnnt.guillaume@gmail.com>
2015-01-23 09:34:19 +0100

    Modif dossier country et début de lecture.

--- dark/pos.lua
+++ dark/pos.lua
@@ -1,23 +1,40 @@
+local name = dark.pipeline()
+--chargement de la liste des prénom
+name:lexicon("&pays", "pays.txt")
+
+local position = dark.pipeline()
+position:pattern([[
+	[&continent (
+			/[Ee]urope/ |
+			/[Aa]sie/ |
+			/[Aa]frique/ |
+			/[Oo]c[ée]anie/ |
+			/[Aa]m[ée]rique/
+			)
+	]
+]])
+
+position:pattern([[
+	[&frontalier ( /entouré/ | /bordé/) par (&CON? &DET? &pays &PCT?)+
+	]
+]])
+
 local main = dark.pipeline()
+--détéction des mots de la langue française
 main:model("model/postag-fr")
 
-main:pattern("[&DATE &NUM/&NUM/&NUM]")
-main:pattern("[&NP &DET? &ADJ* (&NNC | &NNP)+ &ADJ* ]")
-
-main:pattern("[&PER &NND* (&NNP | &part | /^%u%a*$/ )]")
-
+--ajout des pattern créés précédemments
+main:add(name)
+main:add(position)
 
 local tag = {
-	ADJ = "black", ADP = "black", ADV = "black", CON = "black",
-	DET = "red", NNC = "black", NNP = "green", NUM = "black",
-	OTH = "black", PCT = "black", PRO = "black", PRT = "black",
-	VRB = "blue", NP = "yellow",
-	ORG = "red", PER = "green", MISC = "yellow", LOC = "blue",
-	DATE = "magenta", TITLE = "yellow"
+	pays = "green",
+	continent = "red",
+	frontalier = "blue"
 }
 
 for line in io.lines() do
-	local seq = main(line:gsub("%p", "%1"))
-	seq:dump()
+	local seq = main(line:gsub("[/.\",;]", " %1 "):gsub("[']", "%1 "))
+	--seq:dump()
 	print(seq:tostring(tag))
 end
