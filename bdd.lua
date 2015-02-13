1d08d6e6f7c55cd7f5b85c43ee61835cd334768c
Berelis <mnnt.guillaume@gmail.com>
2015-02-13 11:39:37 +0100

    Y a des meuchant ils ont cassé le PC de Luc, du coup je commit pour lui

--- bdd.lua
+++ bdd.lua
@@ -1,74 +1,26 @@
 bdd = {
-  afghanistan = {
-    regime = "",
-    position = {
-    },
-    capitale = "",
-    religion = {
-    },
-    monnaie = "",
-    ["pays_frontalier"] = {
-      [5] = "ouzbékistan",
-      [1] = "pakistan",
-      [2] = "chine",
-      [3] = "tadjikistan",
-      [4] = "turkménistan",
-      [6] = "iran",
-    },
-    continent = "asie",
-    superficie = "",
-    population = "",
-    langue = {
-    },
-  },
-  ["afrique du sud"] = {
-    regime = "",
-    position = {
-    },
-    capitale = "",
-    religion = {
-    },
-    continent = "afrique",
-    ["pays_frontalier"] = {
-      [1] = "namibie",
-      [2] = "botswana",
-      [3] = "zimbabwe",
-      [4] = "mozambique",
-      [5] = "swaziland",
-      [6] = "lesotho",
-    },
-    population = "",
-    superficie = "",
-    monnaie = "",
-    langue = {
-    },
-  },
-  france = {
-    regime = "république constitutionnelle",
-    position = {
-      [1] = "Europe de l'Ouest",
-    },
-    capitale = "paris",
-    religion = {
-      [1] = "",
-    },
-    monnaie = "euro",
-    ["pays_frontalier"] = {
-      [6] = "luxembourg",
-      [1] = "belgique",
-      [3] = "allemagne",
-      [7] = "andorre",
-      [8] = "monaco",
-      [4] = "suisse",
-      [9] = "liechtenstein",
-      [2] = "espagne",
-      [5] = "italie",
-    },
-    continent = "Europe",
-    superficie = "Over 9000",
-    population = "66,6 millions",
-    langue = {
-      [1] = "français",
-    },
-  },
+	france = {
+		langue = {"français"},
+		capitale = "paris",
+		monnaie = "euro",
+		religion = {},
+		regime = "république constitutionnelle",
+		population = "66,6 millions",
+		superficie = "Over 9000",
+		continent = "Europe",
+		position = {"Europe de l'Ouest"},
+		pays_frontalier = {"belgique", "espagne", "allemagne", "suisse", "italie", "luxembourg", "andorre", "monaco", "liechtenstein"},
+	},
+	allemagne = {
+		langue = {"hylien"},
+		capitale = "chateau d'hyrule",
+		monnaie = "rubis",
+		religion ={"polythéiste"},
+		position = {"DTC"},
+		pays_frontalier = {"royaume champignon", "zebes", "kanto"},
+		population = "69 420",
+		regime = "monarchie",
+		continent = "nintendo land",
+		superficie = "987654 km2",
+	},
 }
--- dialogue.lua
+++ dialogue.lua
@@ -10,11 +10,54 @@ local Q = dark.pipeline()
 Q:pattern(
 [[
   [&Q
-     (/qui/) |
-     (/quoi/) |
-     (/combien/) |
-     ((/l[ea]/)?/quel[le]?[s]?/) |
-     (/ou/)     
+    (/lequel/) |
+	(/quel/) |
+	(/quelle/) |
+	(/laquelle/) |
+	(/comment/) |
+	(/lesquels/) |
+	(/quels/) |
+	(/lesquelles/) |
+	(/quelles/) |
+	(/qui/) |
+	(/o[uù]/) |
+	(/combien/)
+  ]
+]])
+
+local QPluriel = dark.pipeline()
+QPluriel:pattern(
+[[
+  [&QPluriel
+	(/comment/) |
+	(/lesquels/) |
+	(/quels/) |
+	(/lesquelles/) |
+	(/quelles/)
+  ]
+]])
+
+local QPerson = dark.pipeline()
+QPerson:pattern(
+[[
+  [&QPerson
+     (/qui/)
+  ]
+]])
+
+local QLieu = dark.pipeline()
+QLieu:pattern(
+[[
+  [&QLieu
+     (/o[uù]/)
+  ]
+]])
+
+local QChiffre = dark.pipeline()
+QChiffre:pattern(
+[[
+  [&QChiffre
+     (/combien/)
   ]
 ]])
 
@@ -50,6 +93,7 @@ langue:pattern(
 [[
 	[&langue
 		(/langue[s]?/) |
+		(/parle/) |
 		(/dialecte[s]?/) |
 		(/langage[s]?/)
 	]
@@ -146,6 +190,10 @@ main:add(monnaie)
 main:add(superficie)
 main:add(pays)
 main:add(Q)
+main:add(QPluriel)
+main:add(QPerson)
+main:add(QLieu)
+main:add(QChiffre)
 main:add(religion)
 main:add(regime)
 main:add(continent)
@@ -164,9 +212,14 @@ local tag = {
 	continent = "magenta",
 	pays ="cyan",
 	pays_frontalier = "cyan",
-	Q = "dark"
+	Q = "dark",
+	QPluriel = "dark",
+	QPerson = "dark",
+	QChiffre = "dark",
+	QLieu = "dark"
 }
 
+--Donne les tokens de seq entre de debut et fin
 function get_tokens(seq, debut, fin)
 	local tab = {}
 
@@ -177,6 +230,7 @@ function get_tokens(seq, debut, fin)
 	return table.concat(tab, " ")
 end
 
+--Donne les tag de seq
 function get_tags(seq, tag)
 	local res = {}
 
@@ -185,6 +239,59 @@ function get_tags(seq, tag)
 	end
 
 	return res
+end
+
+-- Si para == true, indique si on a le tag tag dans seq
+-- Sinon on renvoit le nb de tag tag dans seq
+function get_question(seq, tag, para)
+	local res = get_tags(seq, tag)
+
+	if para == true then
+		if res[1] ~= nil then
+			res = true
+		else
+			res = false
+		end
+	else
+		if res[1] ~= nil then
+			res = #res
+		else
+			res = 0
+		end
+	end
+
+	return res
+end
+
+-- Trouver la reponse a un sujet et un context
+function get_reponse(i,question)
+	print(serialize(question.sujet))
+	print(i)
+
+	question.reponse[i] = {}
+
+	--Trouver la reponse
+	if bdd[question.contexte] ~= nil and
+	bdd[question.contexte][question.sujet[i]] ~= nil then
+		question.reponse[#question.reponse + 1] = bdd[question.contexte][question.sujet[i]]
+
+		-- Si Reponse vide
+		if #question.reponse[i][1] ~= nil and #question.reponse[i] == 0 then
+			print("Nous n'avons pas la reponse a cette question.")
+		-- Sinon
+		else			
+			if #question.reponse[i] == 1 then
+				print("La reponse a votre question est : " .. question.reponse[i][1])
+			else
+				print("Les reponses a votre question sont :")
+				for j = 1, #question.reponse[i] do
+					print(question.reponse[i][j])
+				end
+			end	
+		end
+	else
+		print("Nous ne gerons pas encore cette question.")
+	end
 end
 
 local listequestions = {}
@@ -230,6 +337,7 @@ repeat
 	local res = get_tags(seq, "&pays")
 
 	if res[1] ~= nil then
+		-- Si la question porte sur un pays
 		print("Le contexte de votre question est : " .. res[1])
 		question.contexte = res[1]
 	elseif listequestions[questionnumber-1] ~= nil and
@@ -245,11 +353,60 @@ repeat
 
 	--Determiner le type de reponse via Q
 
+	--Nombre de question
+	local nb_question = get_question(seq, "&Q",false)
+	--Si la question porte sur des personnes 
+	local person = get_question(seq, "&QPerson",true)
+	--Si la question porte sur un lieu
+	local lieu = get_question(seq, "&QLieu",true)
+	--Si la question porte sur des chiffres
+	local chiffre = get_question(seq, "&QChiffre",true)
+	--Si la question attend plusieurs réponses
+	local pluriel = get_question(seq, "&QPluriel",true)
+
 ----------------------------------------------------------------------------------------------------
 	
 	if question.contexte ~= nil then
 
 		--Trouver ce sur quoi porte la question
+		question.sujet = {}
+
+		for k, v in pairs(tokentag) do
+			for K , V in pairs(bdd[question.contexte]) do
+				if (v:gsub("&", "") == K) then
+					question.sujet[#question.sujet + 1] = K;
+					break;
+				end
+			end
+		end
+
+		print(serialize(question.sujet))
+
+		if question.sujet[1] ~= nil then
+			if #question.sujet == 1 then
+				print("Le sujet de votre question est : " .. question.sujet[1])
+			else
+				print("Les sujets de votre question sont : ")
+				for i = 1, #question.sujet do	
+					print("-->" .. question.sujet[i])
+				end
+			end
+		elseif listequestions[questionnumber-1] ~= nil and
+		listequestions[questionnumber-1].sujet[1] ~= nil then
+			if #listequestions[questionnumber-1].sujet == 1 then
+				print("Le sujet de votre question est le même que celui de la question précédante : " .. listequestions[questionnumber-1].sujet[1])
+			else
+				print("Les sujets de votre question sont les mêmes que ceux de la question précédante: ")
+				for i = 1, #listequestions[questionnumber-1].sujet do	
+					print("-->" .. listequestions[questionnumber-1].sujet[i])
+				end
+			end
+		else
+			print("Votre question ne porte sur rien")
+			question.sujet = nil
+		end
+
+		--[[
 		for k, v in pairs(tokentag) do
 			for K , V in pairs(bdd[question.contexte]) do
 				if (v:gsub("&", "") == K) then
@@ -260,7 +417,7 @@ repeat
 		end
 
 		if question.sujet ~= nil then
-			print("Le champs de votre question est : " .. question.sujet)
+			print("Le sujet de votre question est : " .. question.sujet)
 		elseif listequestions[questionnumber-1] ~= nil and
 		listequestions[questionnumber-1].sujet ~= nil then
 			print("Votre question a pour sujet celui de la question precedante : " .. listequestions[questionnumber-1].sujet)
@@ -270,19 +427,23 @@ repeat
 			question.sujet = nil
 		end
 
+		--]]
+
 ----------------------------------------------------------------------------------------------------
 
-		if question.sujet ~= nil then
+		if question.sujet[1] ~= nil then
 
-			--Trouver la reponse
-			if bdd[question.contexte] ~= nil and
-			bdd[question.contexte][question.sujet] ~= nil then
-				question.reponse = bdd[question.contexte][question.sujet]
-				print("La reponse a votre question est : " .. question.reponse)
+			question.reponse = {}
+			
+			--Un sujet dans la question
+			if #question.sujet == 1 then
+				get_reponse(1,question)
+			--Plusieurs sujets dans la question
 			else
-				print("Nous n'avons pas la reponse ou nous ne la gerons pas encore")
+				for i = 1, #question.sujet do
+					get_reponse(i,question)
+				end
 			end
-
 		end
 
 	end
